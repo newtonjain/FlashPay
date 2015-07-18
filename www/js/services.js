1754f2d4ca8dc258b34ee58b2aaf52324ec6fc81
Newton Jain <newton.jain@gmail.com>
2015-07-18 15:56:23 -0400

    went inside

--- www/js/services.js
+++ www/js/services.js
@@ -6,30 +6,33 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    name: 'TIAN YUAN ZHAO',
+    date: '18/07/2015',
+    time: '13:00 EST',
+    amount: '$27.45',
+    face: 'img/Icon---Mastercard.png'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+    name: 'NEWTON JAIN ',
+    date: '18/07/2015',
+    time: '13:00 EST',
+    amount: '$27.45',
+    face: 'img/Icon---VISA.png'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+    name: 'DANIEL MIRMILSHTEYN',
+    date: '18/07/2015',
+    time: '13:00 EST',
+    amount: '$27.45',
+    face: 'img/Icon---Interac.png'
   }, {
     id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
+    name: 'SHIVA KAUSHAL',
+    date: '18/07/2015',
+    time: '13:00 EST',
+    amount: '$27.45',
+    face: 'img/Icon---AMEX.png'
+  }, ];
 
   return {
     all: function() {
