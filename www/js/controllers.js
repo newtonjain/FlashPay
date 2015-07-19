e7850d673790dc0141b3b5770973c95a37b8136c
Newton Jain <newton.jain@gmail.com>
2015-07-18 21:50:56 -0400

    Added test for braintree api

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,135 +1,89 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope) {
-  alert('here');
-  //document.addEventListener('deviceready', this.onDeviceReady, false);
-  $scope.tapping = function(){
-    $scope.app.initialize();
-  };
+.controller('AppCtrl', function ($scope, $http) {
+    $scope.creditCard = {};
 
-$scope.app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-          document.addEventListener('deviceready', this.onDeviceReady, false);
-
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-      alert('1');
-        // When the device is ready, hide the listening element
-        app.hideElement('.listening');
-
-        // Display that the device is ready
-        app.showElement('.received');
-
-        // Initialize Triangle
-        // TODO: change these values to values you obtain for your own application from triangle.io
-        navigator.triangle.initialize(
-            "vZbpthAY7lCMEQF", // application ID
-            "JyA9Qbil4E", // access key
-            "O7ZiSeoLFzUs0M7zoJl5IsKrNtNTDJaMUw6AXMCiV6NYIgxN2gMzZZVmnxvpqv7W", // secret key
-            function () // success callback
-            {
-                // Hide that the device is ready, it's given now
-                app.hideElement('.received');
-                alert('I am here too');
-                // Display the Triangle ready element
-                app.showElement('.triangle-ready');
-
-                // Subscribe to events that the Triangle APIs raise
-                document.addEventListener('ontaperror', app.onTapError, false);
-                document.addEventListener('ontapdetect', app.onTapDetect, false);
-                document.addEventListener('ontapsuccess', app.onNewCard, false);
-            },
-            function (message) // error callback
-            {
-                console.log("there was an error initializing the Triangle APIs");
-                console.error(message);
-            }
-        );
-    },
-    showElement: function (css)
-    {
-        var parentElement = document.getElementById('notifications');
-        var element = parentElement.querySelector(css);
-
-        element.setAttribute('style', 'display:block');
-    },
-    hideElement: function (css)
-    {
-        var parentElement = document.getElementById('notifications');
-        var element = parentElement.querySelector(css);
-
-        element.setAttribute('style', 'display:none');
-    },
-    onNewCard: function (card)
-    {
-        console.log("Scanned card successfully.");
-
-        // Display basic card information to the user
-        // various other properties such as cardholderName,
-        // activationDate, expiryDate, cardPreferredName, and encryptedAccountNumber
-        // may be available.
-        var dataToShow = card.cardBrand;
-        if (card.cardholderName != undefined)
-        {
-            dataToShow += "\n" + card.cardholderName;
-        }
-        dataToShow += "\n**** **** **** " + card.lastFourDigits;
-
-        alert(dataToShow);
-    },
-    onTapDetect: function ()
-    {
-        console.log("Detected new tap.");
-    },
-    onTapError: function(error)
-    {
-        console.log("Error processing contactless card.");
-        console.error(error);
+    var goodNCard = {
+        number: '4111111111111111',
+        expirationDate: '08/16',
+        amount: '99'
     }
-};
-  
+    //document.addEventListener('deviceready', this.onDeviceReady, false);
+    $scope.tapping = function () {
+
+        nfc.addTagDiscoveredListener(
+          function (nfcEvent) {
+              $scope.tag = nfcEvent.tag;
+              alert(JSON.stringify($scope.tag.id));
+              assignCard($scope.tag);
+          },
+          function () {
+              alert("Tap your card please.");
+          },
+          function (reason) {
+              console.log("Error: " + reason);
+          }
+        );
+    };
+
+    var assignCard = function (tag) {
+        if ($scope.tag.id[0] == -49 && $scope.tag.id[1] == 76 && $scope.tag.id[2] == 98 && $scope.tag.id[3] == 5) {
+            alert('sup');
+            $scope.creditCard = goodNCard;
+        }
+        if ($scope.tag.techTypes.length > 3) {
+            $scope.creditCard.type = 'Visa';
+        }
+        if ($scope.tag.techTypes.length == 3) {
+            $scope.creditCard.type = 'MasterCard';
+        }
+        if ($scope.tag.techTypes.length == 2) {
+            $scope.creditCard.type = 'Interac';
+        }
+
+        $http.post('https://flashpay.herokuapp.com/createPayment', $scope.creditCard)
+        .success(function (data, status, headers, config) {
+            alert('recieved' + data);
+        }).error(function (data, status, headers, config) {
+            alert('There was a problem retrieving your information' + data);
+        });
+    };
+
+    $scope.justcheck = function () {
+        alert(JSON.stringify($scope.creditCard));
+    }
+
 
 })
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function ($scope) {
+
 
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('ChatsCtrl', function ($scope, Chats) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+        Chats.remove(chat);
+    };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+        enableFriends: true
+    };
 
-//  http.$get('flashpay.herokuapp.com/createPayment', params)
+    //  http.$get('flashpay.herokuapp.com/createPayment', params)
 });
